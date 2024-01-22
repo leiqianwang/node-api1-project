a86db531fa658b591ff71acd5e2d089dca11a1ce
Yingying Wang <yxw241@gmail.com>
2024-01-22 14:45:35 -0500

    second commit

--- api/server.js
+++ api/server.js
@@ -4,9 +4,30 @@ const User = require('./users/model')
 //INSTANCE OF EXPRESS APP
 const server = express();
 
-
+server.use(express.json());
 
 //ENDPOINTS
+
+server.post('/api/users', (req, res) => {
+     const user = req.body;
+     if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user',
+        })
+     }else {
+         User.insert(user)
+         .then(postUser => {
+             res.status(201).json(postUser)
+         })
+         .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the user to the database" ,
+                err:  err.message,
+                stack: err.stack,
+            })
+         })
+     }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
