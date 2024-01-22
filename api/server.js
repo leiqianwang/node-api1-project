81106f73c891b1e0dd7b1f056db00c69fe248d71
Yingying Wang <yxw241@gmail.com>
2024-01-22 14:30:45 -0500

    first commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,56 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model')
+//INSTANCE OF EXPRESS APP
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+
+//ENDPOINTS
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+         res.status(500).json({
+            message: "The users information could not be retrieved",
+            err:  err.message,
+            stack: err.stack,
+         })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+         res.status(500).json({
+            message: "error gettng user",
+            err:  err.message,
+            stack: err.stack,
+         })
+    })
+})
+
+//Global middleware
+// server.use('*', (req, res) =>  {
+//        res.status(404).json({
+//         message:   'not found'
+//        })
+// });
+
+server.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Endpoint not found'
+    });
+  });
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+//console.log("hi");
+
+server.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`);
+});
